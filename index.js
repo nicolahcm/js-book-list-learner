79b79b87face6decaf978415e9700e2a08e89049
nicolahcm <nicolahu1997@gmail.com>
2021-01-10 01:26:05 +0100

    2nd better version with classes UI and storage. Missing the cool styling

--- index.js
+++ index.js
@@ -4,69 +4,124 @@ class Book {
     constructor(title, author, editor, id) {
         this.title = title;
         this.author = author;
-        this.editor = editor;
-        this.id = id
+        this.editor = editor
     }
 }
 
 
-// Getting DOM elements
-let submitBtn = document.querySelector('#submitButton')
-let unorderedList = document.querySelector('#unorderedList')
-let searchField = document.querySelector('#searchField')
+class UI {
 
-// setting variables.
-let arrBooks = {}
-let id;
-let keys = []
+    static displayABook(title, author, editor, id) {
+        let sampleBookItem = document.querySelector('#sampleBook').cloneNode(true)
+        sampleBookItem.style.display = "block"
+        sampleBookItem.setAttribute("data-id", `${id}`)
 
+        sampleBookItem.querySelector('#titleDisp').innerHTML = `${title}`
+        sampleBookItem.querySelector('#authorDisp').innerHTML = `${author}`
+        sampleBookItem.querySelector('#editorDisp').innerHTML = `${editor}`
 
-////////////////////////////////////////
-///////////////////  DECLARING FUNCTIONS
-////////////////////////////////////////
-
-function initialization() {
-
-    // Filling keys array and arrBooks, looking in the localStorage for those there yet
-    for (let key in JSON.parse(localStorage.getItem('arrBooks'))) {
-        keys.push(key)
-        arrBooks[key] = JSON.parse(localStorage.getItem('arrBooks'))[key]
+        document.querySelector('#unorderedList').append(sampleBookItem)
     }
 
 
-    // Setting id to insert for new books. 
-    if (keys.length == 0) {
-        id = 0
-    } else {
-        id = Math.max(...keys) + 1
+    static displayAll() {
+        {
+            let arrBooks = Storage.getBooks()
+
+            for (let key in arrBooks) {
+                let book = arrBooks[key]
+                UI.displayABook(book.title, book.author, book.editor, key)
+            }
+        }
+    }
+
+
+
+    static clearOutput() {
+        document.querySelector("#titleField").value = ""
+        document.querySelector('#authorField').value = ""
+        document.querySelector('#editorField').value = ""
+
+    }
+
+
+    static searchAndList(e) {
+        let val = e.target.value.toLowerCase()
+        console.log(val)
+
+        let arrBooks = Storage.getBooks()
+
+        for (let key in arrBooks) {
+            let book = arrBooks[key]
+
+            let title = book.title.toLowerCase()
+            let author = book.author.toLowerCase()
+            let editor = book.editor.toLowerCase()
+            let itemBook = document.querySelector("[data-id=" + CSS.escape(key) + "]")
+
+            if (!(title.includes(val) || author.includes(val) || editor.includes(val))) {
+                itemBook.style.display = "none"
+            } else {
+                itemBook.style.display = "block"
+            }
+        }
+    }
+}
+
+
+class Storage {
+
+    static getBooks() {
+        return JSON.parse(localStorage.getItem('arrBooks'))
+    }
+
+    static addBook(book) {
+        let arrBooks = Storage.getBooks()
+
+
+
+        // Getting the id for pushing
+        let keys = []
+
+        for (let key in arrBooks) {
+            keys.push(key)
+        }
+
+
+        let id;
+
+        if (keys.length == 0) {
+            id = 0
+        } else {
+            id = Math.max(...keys) + 1
+        }
+
+        // Pushing
+        arrBooks[id] = book
+
+        // Inserting in localStorage
+        localStorage.setItem('arrBooks', JSON.stringify(arrBooks))
+
+        console.log('id is', id)
+        return id
+    }
+
+
+    static deleteBook(id) {
+        let arrBooks = Storage.getBooks()
+        delete arrBooks[id]
+        localStorage.setItem('arrBooks', JSON.stringify(arrBooks))
     }
 }
 
 
 
-// Takes as input title, value, editor. These are strings.
-// id is a number.
-function createUIBook(title, author, editor, id) {
-    let sampleBookItem = document.querySelector('#sampleBook').cloneNode(true)
-    sampleBookItem.style.display = "block"
-    sampleBookItem.setAttribute("data-id", `${id}`)
 
-    sampleBookItem.querySelector('#titleDisp').innerHTML = `${title}`
-    sampleBookItem.querySelector('#authorDisp').innerHTML = `${author}`
-    sampleBookItem.querySelector('#editorDisp').innerHTML = `${editor}`
-
-    document.querySelector('#unorderedList').append(sampleBookItem)
+function deleteAllBooks() {
+    localStorage.setItem('arrBooks', JSON.stringify({}))
 }
 
 
-function displayAtReload() {
-    let arrBooks = JSON.parse(localStorage.getItem('arrBooks'))
-
-    for (let key in arrBooks) {
-        let book = arrBooks[key]
-        createUIBook(book.title, book.author, book.editor, book.id)
-    }
-}
 
 
 /////////////////////
@@ -75,68 +130,37 @@ function displayAtReload() {
 function getFieldsAndAdd(e) {
 
     e.preventDefault()
-
-
     // Grabbing the fields
     let title = document.querySelector("#titleField");
     let author = document.querySelector('#authorField');
     let editor = document.querySelector('#editorField');
-
-
     // Create the book object and inserting into the array and local storage.
-    let book = new Book(title.value, author.value, editor.value, id)
-    arrBooks[id] = book
-    localStorage.setItem('arrBooks', JSON.stringify(arrBooks))
+    let book = new Book(title.value, author.value, editor.value)
 
 
-    createUIBook(book.title, book.value, book.editor, book.id)
-    id = id + 1
+    // Adding Book to the Storage and getting the ID for displaying in the UI
+    let id = Storage.addBook(book)
 
-
+    // Adding Book to the UI (ID needed)
+    UI.displayABook(book.title, book.author, book.editor, id)
 
     // Cleaning fields
-    title.value = ""
-    author.value = ""
-    editor.value = ""
+    UI.clearOutput()
 }
 
 
 function deletingBooks(e) {
 
-    for (val of e.target.classList) {
-        if (val === "delete") {
+    if (e.target.classList.contains('delete')) {
+        let parentLi = e.target.parentElement
+        console.log(parentLi)
+        let bookId = parentLi.getAttribute('data-id')
 
-            let parentLi = e.target.parentElement
-            let bookId = parentLi.getAttribute('data-id')
+        // Removing from the Storage.
+        Storage.deleteBook(bookId)
 
-            delete arrBooks[bookId]
-            localStorage.setItem('arrBooks', JSON.stringify(arrBooks))
-            parentLi.remove()
-        }
-    }
-}
-
-
-
-function searchAndList(e) {
-    let val = e.target.value.toLowerCase()
-    console.log(val)
-
-
-    for (let key in arrBooks) {
-        let book = arrBooks[key]
-
-        let title = book.title.toLowerCase()
-        let author = book.author.toLowerCase()
-        let editor = book.editor.toLowerCase()
-        let itemBook = document.querySelector("[data-id=" + CSS.escape(book.id) + "]")
-
-        if (!(title.includes(val) || author.includes(val) || editor.includes(val))) {
-            itemBook.style.display = "none"
-
-        } else {
-            itemBook.style.display = "block"
-        }
+        // Removng in the UI
+        parentLi.remove()
     }
 }
 
@@ -147,20 +171,14 @@ function searchAndList(e) {
 
 
 
-
-
-// Reading the local storage
-initialization()
-
-// Display if there is some data in local storage
-displayAtReload()
-
+// Display at reload
+document.addEventListener('DOMContentLoaded', UI.displayAll)
 
 // Adding books.
-submitBtn.addEventListener('click', getFieldsAndAdd)
+document.querySelector('#submitButton').addEventListener('click', getFieldsAndAdd)
 
 // Deleting books.
-unorderedList.addEventListener('click', deletingBooks)
+document.querySelector('#unorderedList').addEventListener('click', deletingBooks)
 
 // Searching.
-searchField.addEventListener('keyup', searchAndList)
+document.querySelector('#searchField').addEventListener('keyup', UI.searchAndList)
